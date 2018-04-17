de592a0b1be4e6f56b36df79b8de8ecf74fa1e4f
djsegal <dansegal2@gmail.com>
2018-04-17 04:37:24 -0400

    Clean up K_FT func to remove repeat epsilon calls

--- src/methods/reactors/coeffs/k_ft.jl
+++ src/methods/reactors/coeffs/k_ft.jl
@@ -1,4 +1,6 @@
 function K_FT(cur_reactor::AbstractReactor)
+  cur_epsilon = cur_reactor.epsilon
+
   cur_K = 6.8e-8
 
   cur_K *= cur_reactor.C_saw
@@ -9,9 +11,10 @@ function K_FT(cur_reactor::AbstractReactor)
 
   cur_K /= kappa_x(cur_reactor)
 
-  cur_K /= cur_reactor.epsilon ^ 2
+  cur_K /= cur_epsilon ^ 2
 
-  cur_K *= ( 4.3 - 0.6 / cur_reactor.epsilon )
+  cur_K *= ( 4.3 - 0.6 / cur_epsilon )
+
 
   cur_K
 end
