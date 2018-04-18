64f670696dfc6e30ad13d6ef80972fad1a04b323
djsegal <dansegal2@gmail.com>
2018-04-18 18:43:21 -0400

    Allow all standard reactor fields to be symbolic

--- src/structs/reactor.jl
+++ src/structs/reactor.jl
@@ -12,40 +12,40 @@
   is_pulsed::Bool = true
   is_symbolic::Bool = false
 
-  H::AbstractFloat = 1.0
-  Q::AbstractFloat = 39.86
+  H::AbstractSymbol = 1.0
+  Q::AbstractSymbol = 39.86
 
-  epsilon::AbstractFloat = 0.3226
-  kappa_95::AbstractFloat = 1.590
-  delta_95::AbstractFloat = 0.333
+  epsilon::AbstractSymbol = 0.3226
+  kappa_95::AbstractSymbol = 1.590
+  delta_95::AbstractSymbol = 0.333
 
-  nu_n::AbstractFloat = 1.00
-  nu_T::AbstractFloat = 1.45
+  nu_n::AbstractSymbol = 1.00
+  nu_T::AbstractSymbol = 1.45
 
-  Z_eff::AbstractFloat = 2.584
-  f_D::AbstractFloat = 0.9
+  Z_eff::AbstractSymbol = 2.584
+  f_D::AbstractSymbol = 0.9
 
-  A::AbstractFloat = 2.5
+  A::AbstractSymbol = 2.5
 
-  l_i::AbstractFloat = 1.155
-  rho_m::AbstractFloat = 0.6
+  l_i::AbstractSymbol = 1.155
+  rho_m::AbstractSymbol = 0.6
 
-  N_G::AbstractFloat = 1.2
+  N_G::AbstractSymbol = 1.2
 
-  eta_CD::AbstractFloat = 0.256
-  eta_T::AbstractFloat = 0.352
+  eta_CD::AbstractSymbol = 0.256
+  eta_T::AbstractSymbol = 0.352
 
-  tau_FT::AbstractFloat = 7200.0
-  C_saw::AbstractFloat = 1.0
-  C_ejima::AbstractFloat = 0.3
+  tau_FT::AbstractSymbol = 7200.0
+  C_saw::AbstractSymbol = 1.0
+  C_ejima::AbstractSymbol = 0.3
 
-  B_OH::AbstractFloat = 12.8
-  R_OH::AbstractFloat = 2.89
-  dR_OH::AbstractFloat = 0.82
+  B_OH::AbstractSymbol = 12.8
+  R_OH::AbstractSymbol = 2.89
+  dR_OH::AbstractSymbol = 0.82
 
-  max_beta_N::AbstractFloat = 0.02591
-  max_q_95::AbstractFloat = 3.247
-  max_P_E::AbstractFloat = 1000.0
+  max_beta_N::AbstractSymbol = 0.02591
+  max_q_95::AbstractSymbol = 3.247
+  max_P_E::AbstractSymbol = 1000.0
 
   beta_N::AbstractCalculated = nothing
   q_95::AbstractCalculated = nothing
@@ -67,9 +67,19 @@ function Reactor(cur_temp::AbstractSymbol; cur_kwargs...)
 end
 
 function SymbolicReactor(; cur_kwargs...)
-  Reactor(
+  cur_reactor = Reactor(
     is_symbolic = true,
     mode_scaling = symbol_scaling,
     cur_kwargs...
   )
+
+  for cur_field_name in fieldnames(cur_reactor)
+    cur_field = getfield(cur_reactor, cur_field_name)
+    isa(cur_field, AbstractFloat) || continue
+
+    cur_field = symbols(cur_field_name)
+    setfield!(cur_reactor, cur_field_name, cur_field)
+  end
+
+  cur_reactor
 end
