edff99f4e749329f75bc5045ebd5a8249b7c64bb
djsegal <dansegal2@gmail.com>
2018-04-20 05:41:09 -0400

    Store deck names inside Sweep data structure

--- src/structs/sweep.jl
+++ src/structs/sweep.jl
@@ -1,6 +1,8 @@
 mutable struct Sweep <: AbstractSweep
   T_bar_list::Vector{AbstractFloat}
 
+  deck::Union{Void, Symbol}
+
   beta_reactors::Vector{AbstractReactor}
   kink_reactors::Vector{AbstractReactor}
   pcap_reactors::Vector{AbstractReactor}
@@ -9,8 +11,12 @@ end
 function Sweep(cur_T_bar_list::Any; cur_kwargs...)
   cur_dict = Dict(cur_kwargs)
 
+  cur_deck = haskey(cur_dict, :deck) ?
+    cur_dict[:deck] : nothing
+
   cur_sweep = Sweep(
-    cur_T_bar_list, [], [], []
+    cur_T_bar_list, cur_deck,
+    [], [], []
   )
 
   for cur_limit in keys(secondary_limits)
