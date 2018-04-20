2875ce1344f61701786221cfb3d7353adebe52b4
djsegal <dansegal2@gmail.com>
2018-04-20 05:18:43 -0400

    Add utility first/last count to get_limit_vars

--- src/methods/sweeps/get_limit_vars.jl
+++ src/methods/sweeps/get_limit_vars.jl
@@ -1,4 +1,4 @@
-function get_limit_vars(cur_sweep::AbstractSweep, cur_limit::Symbol, cur_field::Symbol)
+function get_limit_vars(cur_sweep::AbstractSweep, cur_limit::Symbol, cur_field::Symbol; cur_first::Union{Void,Int}=nothing, cur_last::Union{Void,Int}=nothing)
   cur_field_symbol = Symbol("$(cur_limit)_reactors")
 
   cur_reactor_list = getfield(cur_sweep, cur_field_symbol)
@@ -12,6 +12,14 @@ function get_limit_vars(cur_sweep::AbstractSweep, cur_limit::Symbol, cur_field::
       cur_values,
       getfield(cur_reactor, cur_field)
     )
+  end
+
+  cur_length = length(cur_values)
+
+  if cur_first != nothing && cur_first < cur_length
+    return cur_values[1:cur_first]
+  elseif cur_last != nothing && cur_last < cur_length
+    return cur_values[end-(cur_last-1):end]
   end
 
   cur_values
