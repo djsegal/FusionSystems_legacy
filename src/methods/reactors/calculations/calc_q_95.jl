204ac8d6dea346d1850151808da2c663fbd3929c
djsegal <dansegal2@gmail.com>
2018-04-21 16:36:22 -0400

    Go back to basics for calculating q_95 from params

--- src/methods/reactors/calculations/calc_q_95.jl
+++ src/methods/reactors/calculations/calc_q_95.jl
@@ -1,13 +1,15 @@
 function calc_q_95(cur_reactor::AbstractReactor)
-  cur_q_95 = K_KF(cur_reactor)
+  cur_q_95 = 5.0
 
-  cur_q_95 /= cur_reactor.max_q_95
+  cur_q_95 *= cur_reactor.B_0
 
-  cur_q_95 *= G_KF(cur_reactor)
+  cur_q_95 *= a(cur_reactor) ^ 2
 
-  cur_q_95 /= cur_reactor.B_0
+  cur_q_95 *= _f_q(cur_reactor)
 
-  cur_q_95 ^= -1
+  cur_q_95 /= cur_reactor.R_0
+
+  cur_q_95 /= cur_reactor.I_P
 
   cur_q_95
 end
