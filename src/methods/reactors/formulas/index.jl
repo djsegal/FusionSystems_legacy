c294ecd5e50b1a9093d0bfcf0462cb31094399aa
djsegal <dansegal2@gmail.com>
2018-04-21 16:47:50 -0400

    Move free methods into sizings and formulas dirs

---
+++ src/methods/reactors/formulas/index.jl
@@ -0,0 +1,13 @@
+include("n_rho.jl")
+include("t_rho.jl")
+
+include("p_bar.jl")
+
+include("tau_e.jl")
+
+include("b_p.jl")
+
+include("phi_total.jl")
+
+include("magnetic_energy.jl")
+include("cost.jl")
---
+++ src/methods/reactors/sizings/index.jl
@@ -0,0 +1,15 @@
+include("a.jl")
+
+include("geom_factor.jl")
+
+include("alpha_i_star.jl")
+include("alpha_n_star.jl")
+include("alpha_r_star.jl")
+
+include("delta_x.jl")
+include("kappa_x.jl")
+
+include("kappa_tau.jl")
+
+include("surface_area.jl")
+include("volume.jl")
