844358208c68903aece2e523bdfda932379821e6
djsegal <dansegal2@gmail.com>
2018-04-21 23:52:43 -0400

    Improve robustness of calc_frac for: CD and ID

--- src/methods/reactors/calculations/calc_f_cd.jl
+++ src/methods/reactors/calculations/calc_f_cd.jl
@@ -1,5 +1,10 @@
 function calc_f_CD(cur_reactor::AbstractReactor)
-  cur_f_CD = cur_reactor.eta_CD
+  cur_eta_CD = cur_reactor.eta_CD
+
+  ( cur_eta_CD == nothing ) &&
+    ( cur_eta_CD = calc_eta_CD(cur_reactor) )
+
+  cur_f_CD = cur_eta_CD
 
   cur_f_CD *= P_H(cur_reactor)
 
@@ -8,6 +13,12 @@ function calc_f_CD(cur_reactor::AbstractReactor)
   cur_f_CD /= cur_reactor.n_bar
 
   cur_f_CD /= cur_reactor.R_0
+
+  if isa(cur_reactor.sigma_v, SymEngine.Basic)
+    cur_f_CD = float(
+      calc_sigma_v(cur_reactor, cur_f_CD)
+    )
+  end
 
   cur_f_CD
 end
--- src/methods/reactors/calculations/calc_f_id.jl
+++ src/methods/reactors/calculations/calc_f_id.jl
@@ -1,9 +1,18 @@
 function calc_f_ID(cur_reactor::AbstractReactor)
+  cur_f_BS = cur_reactor.f_BS
+  cur_f_CD = cur_reactor.f_CD
+
+  ( cur_f_BS == nothing ) &&
+    ( cur_f_BS = calc_f_BS(cur_reactor) )
+
+  ( cur_f_CD == nothing ) &&
+    ( cur_f_CD = calc_f_CD(cur_reactor) )
+
   cur_f_ID = 1.0
 
-  cur_f_ID -= cur_reactor.f_BS
+  cur_f_ID -= cur_f_BS
 
-  cur_f_ID -= cur_reactor.f_CD
+  cur_f_ID -= cur_f_CD
 
   cur_eps = eps()
 
