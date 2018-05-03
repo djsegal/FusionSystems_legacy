2c8ee341953c61d1c1741d70ca37ab2a54e682cb
djsegal <dansegal2@gmail.com>
2018-05-02 23:49:41 -0400

    Add arc and act 1 & 2 decks to the list // woo!

---
+++ src/decks/act_1_deck.jl
@@ -0,0 +1,32 @@
+function act_1_deck(cur_T_bar::AbstractSymbol)
+  BaseReactor(
+    T_bar = cur_T_bar,
+    is_pulsed = false,
+    H = 1.65,
+    Q = 42.5,
+    epsilon = 0.25,
+    kappa_95 = 1.964,
+    delta_95 = 0.42,
+    nu_n = 0.27,
+    nu_T = 1.15,
+    nu_n_offset = 0.0,
+    nu_T_offset = 0.0,
+    Z_eff = 2.11,
+    f_D = 0.75,
+
+    A = 2.5,
+    l_i = 0.47,
+
+    N_G = 1.0,
+    eta_T = 0.575,
+    eta_RF = 0.4,
+
+    max_beta_N = 0.056,
+    max_q_95 = 4.5,
+
+    max_P_E = 2500.0,
+
+    max_P_W = 2.45,
+    max_q_DV = 13.7
+  )
+end
---
+++ src/decks/arc_deck.jl
@@ -0,0 +1,32 @@
+function arc_deck(cur_T_bar::AbstractSymbol)
+  BaseReactor(
+    T_bar = cur_T_bar,
+    is_pulsed = false,
+    H = 1.8,
+    Q = 13.6,
+    epsilon = 0.3333,
+    kappa_95 = 1.84,
+
+    delta_95 = 0.333,
+
+    nu_n = 0.385,
+    nu_T = 0.929,
+    nu_n_offset = 0.0,
+    nu_T_offset = 0.0,
+    Z_eff = 1.2,
+
+    f_D = 0.9,
+    A = 2.5,
+
+    l_i = 0.67,
+    N_G = 0.67,
+    eta_T = 0.4,
+    eta_RF = 0.5,
+
+    max_beta_N = 0.0259,
+    max_q_95 = 7.2,
+    max_P_E = 2500.0,
+    max_P_W = 2.5,
+    max_q_DV = 10.0
+  )
+end
